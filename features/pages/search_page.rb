9938a5496dd5636ca442e4f58a1c532ee885b0e0
Michael Collins <mike@iammike.org>
2019-04-07 20:13:55 -0700

    Added verification of results in downloaded file, removed caching

--- features/pages/search_page.rb
+++ features/pages/search_page.rb
@@ -1,39 +1,25 @@
 class SearchPage < SitePrism::Page
 
-  def page_loaded?
-    page.should has_selector?('Filter')
-  end
+  def change_baths_value(action, steps)
+    action = (action == 'increase') ? 'up' : 'down'
+    elem_button = find("#filterContent > div > div:nth-child(1) > div.doubleRow > div:nth-child(2) > div.filterRow > span > span > span.step-#{action}")
 
-  def results_valid?(type, comparison, expected_value)
-    get_all_homes_data
-    if type.include?('Price')
-      expected_value = NumUtils.convert_abbr_to_full(expected_value)
-      expected_value = NumUtils.remove_money_symbols(expected_value).to_i
+    steps.times do
+      elem_button.click
     end
-
-    @homes_data.each do |stats|
-      if type.include?('Price')
-        value = stats[/(\S*)/]
-        value = NumUtils.remove_money_symbols(value).to_i
-      else
-        value = stats[/(\d*\.?\d*) #{type}/,1]
-      end
-      next if value.nil?
-      return false if comparison == 'greater' && expected_value > value
-      return false if comparison == 'less' && expected_value < value
-    end
-    true
+    page_loaded?
   end
 
   def get_all_homes_data
-    if @homes_data.nil? # Caching because this could get expensive
-      elems_home_stats = all('div.HomeStatsV2')
-      elems_home_prices = all('span.homecardV2Price')
-      home_prices = elems_home_prices.map(&:text)
-      home_stats = elems_home_stats.map(&:text)
-      @homes_data = home_prices.zip(home_stats).map { |a| a.join(' ') }
-    end
-    @homes_data
+    elems_home_stats = all('div.HomeStatsV2')
+    elems_home_prices = all('span.homecardV2Price')
+    home_prices = elems_home_prices.map(&:text)
+    home_stats = elems_home_stats.map(&:text)
+    @homes_data = home_prices.zip(home_stats).map { |a| a.join(' ') }
+  end
+
+  def page_loaded?
+    page.should has_selector?('Filter')
   end
 
   def remove_all_filters_from_url
@@ -48,6 +34,61 @@ class SearchPage < SitePrism::Page
     remove_all_filters_from_url + '/filter/' + new_filters
   end
 
+  def results_valid_in_file?(type, comparison, expected_value)
+    latest_csv_path = Dir.glob("#{Dir.home}/Downloads/redfin*").max_by {|f| File.mtime(f)}
+    homes_table = CSV.read(latest_csv_path, headers: true)
+    if type.include?('Price')
+      type = 'Price'
+      expected_value = NumUtils.convert_abbr_to_full(expected_value)
+      expected_value = NumUtils.remove_money_symbols(expected_value).to_i
+    end
+
+    type_data = homes_table[type.upcase]
+    type_data.each do |value|
+      next if value.nil? # Redfin includes results when there's no data of that type
+      return false if comparison == 'greater' && expected_value.to_f > value.to_f
+      return false if comparison == 'less' && expected_value.to_f < value.to_f
+    end
+    true
+  end
+
+  def results_valid_on_screen?(type, comparison, expected_value)
+    get_all_homes_data
+    if type.include?('Price')
+      expected_value = NumUtils.convert_abbr_to_full(expected_value)
+      expected_value = NumUtils.remove_money_symbols(expected_value).to_i
+    end
+
+    @homes_data.each do |stats|
+      if type.include?('Price')
+        value = stats[/(\S*)/]
+        value = NumUtils.remove_money_symbols(value).to_i
+      else
+        value = stats[/(\d*\.?\d*) #{type}/,1]
+      end
+      next if value.nil? # Redfin includes results when there's no data of that type
+      return false if comparison == 'greater' && expected_value.to_f > value.to_f
+      return false if comparison == 'less' && expected_value.to_f < value.to_f
+    end
+    true
+  end
+
+  def set_baths(target_baths)
+    elem_baths_value = find('#filterContent > div > div:nth-child(1) > div.doubleRow > div:nth-child(2) > div.filterRow > span > span > span.input > span > span')
+    target_baths = target_baths.chop.to_i # Remove + sign
+    current_baths_value = (elem_baths_value.text == 'No min') ? 0 : elem_baths_value.text.chop
+
+    if current_baths_value < target_baths
+      steps = (target_baths - current_baths_value).to_i
+      steps+=1 if current_baths_value <= 1.25 && target_baths > 1 # Have to add a step between 1 and 2 baths for 1.25 baths
+      change_baths_value('increase', steps)
+    elsif current_baths_value > target_baths
+      steps = (current_baths_value - target_baths).to_i
+      steps+=1 if target_baths <= 1.25 && current_baths_value > 1 # Have to add a step between 2 and 1 baths for 1.25 baths
+      change_baths_value('decrease', steps)
+    end
+  end
+
   def set_limit_filter(type, min_or_max, value)
     if value == "No #{min_or_max}"
       current_url.include?(',') ? remove_filter_from_url("#{min_or_max}-#{type}") : visit(remove_all_filters_from_url)
@@ -59,7 +100,6 @@ class SearchPage < SitePrism::Page
           new_url = current_url + '/filter/' + filter_path
       visit(new_url)
     end
-    @homes_data = nil
   end
 
   def toggle_property_type(type)
@@ -86,34 +126,6 @@ class SearchPage < SitePrism::Page
     else
       abort 'Invalid property type'
     end
-    @homes_data = nil
-  end
-
-  def set_baths(target_baths)
-    elem_baths_value = find('#filterContent > div > div:nth-child(1) > div.doubleRow > div:nth-child(2) > div.filterRow > span > span > span.input > span > span')
-    target_baths = target_baths.chop.to_i # Remove + sign
-    current_baths_value = (elem_baths_value.text == 'No min') ? 0 : elem_baths_value.text.chop
-
-    if current_baths_value < target_baths
-      steps = (target_baths - current_baths_value).to_i
-      steps+=1 if current_baths_value <= 1.25 && target_baths > 1 # Have to add a step between 1 and 2 baths for 1.25 baths
-      change_baths_value('increase', steps)
-    elsif current_baths_value > target_baths
-      steps = (current_baths_value - target_baths).to_i
-      steps+=1 if target_baths <= 1.25 && current_baths_value > 1 # Have to add a step between 2 and 1 baths for 1.25 baths
-      change_baths_value('decrease', steps)
-    end
-  end
-
-  def change_baths_value(action, steps)
-    action = (action == 'increase') ? 'up' : 'down'
-    elem_button = find("#filterContent > div > div:nth-child(1) > div.doubleRow > div:nth-child(2) > div.filterRow > span > span > span.step-#{action}")
-
-    steps.times do
-      elem_button.click
-    end
-    page_loaded?
-    @homes_data = nil
   end
 
 end
--- features/step_definitions/search_page_steps.rb
+++ features/step_definitions/search_page_steps.rb
@@ -11,6 +11,14 @@ Then(/^I set (.*) filter to (.*)$/) do |filter, value|
   end
 end
 
-And(/^I verify in search results (.*) (?:is|are) (greater|less) than or equal to (.*)$/) do |type, operator, value|
-  expect(@search_page.results_valid?(type, operator, value)).to eq true
+And(/^I verify in search results (.*) (?:is|are) (greater|less) than or equal to (.*) on screen$/) do |type, operator, value|
+  expect(@search_page.results_valid_on_screen?(type, operator, value)).to eq true
+end
+
+And(/^I verify in search results (.*) (?:is|are) (greater|less) than or equal to (.*) in the results file$/) do |type, operator, value|
+  expect(@search_page.results_valid_in_file?(type, operator, value)).to eq true
+end
+
+Then(/^I download the CSV Results file$/) do
+  step %{I click Download}
 end
